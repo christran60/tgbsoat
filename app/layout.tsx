df43ec8e5e3891db470efe0007402e481f1c76d8
Christopher Tran <ctran209@outlook.com>
2025-03-12 22:55:19 -0700

    fix: removed unnecessary usings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Domine } from "next/font/google";
+import { Domine } from "next/font/google";
 import "./globals.css";
 
 const domine = Domine({
